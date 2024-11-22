7bdc637f0b7d7717a5a09149d9d5fb40d4b65571
Joel Galán Pérez <joelgalanperez@gmail.com>
2024-11-22 13:29:53 +0100

    ICO Subido y Logo añadido junto a colores

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-3">
+    <footer style={{ backgroundColor: '#F26F63' }} className="text-white text-center py-3">
       <p>© 2024 StudentConnect. Todos los derechos reservados.</p>
     </footer>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../assets/logo.png';  // Asegúrate de que la ruta sea correcta
+
 
 const AppNavbar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar style={{ backgroundColor: '#F26F63' }} variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">StudentConnect</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          {/* Logo */}
+          <img src={logo} alt="Logo" style={{ width: '40px', marginRight: '10px' }} />
+          {/* Nombre de la marca */}
+          StudentConnect
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
